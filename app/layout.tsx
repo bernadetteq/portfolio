b7ea0579ec64bead378e64b120f6aa897d43a336
Bernadette Quitoriano <bernadettequitoriano@mac.speedport.ip>
2026-05-19 16:53:58 +0200

    google analytics v1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,14 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Inter } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google'
-
-
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-// export const metadata = {
-//   title: "Bernadette Quitoriano's Portfolio",
-//   icons: {
-//     icon: "/favicon.svg",
-//   },
-//   viewport: "width=device-width, initial-scale=1",
-// };
+
 export const metadata = {
   title: "Bernadette Quitoriano Portfolio",
   description: "Bernadette Quitoriano - Product Manager Portfolio",
@@ -46,6 +39,18 @@ export default function RootLayout({
         </div>
         <Footer />
       </body>
+      <Script
+        id="clarity"
+        strategy="afterInteractive"
+      >
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wtlhvpm4em");
+        `}
+      </Script>
       <GoogleAnalytics gaId="G-F7XL718W47" />
     </html>
   );
