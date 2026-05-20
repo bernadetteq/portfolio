c1e3b49b6a3c1c08db69f54bf34afc75f13e8e0a
Bernadette Quitoriano <bernadettequitoriano@mac.speedport.ip>
2026-05-21 01:54:07 +0200

    disable google analytics on localhost

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
       </head>
       <body className="antialiased">
         <div className="max-w-4xl mx-auto max-sm:px-6">
@@ -41,22 +40,24 @@ export default function RootLayout({
           {children}
         </div>
         <Footer />
-      </body>
-      <Script
-        id="clarity"
-        strategy="afterInteractive"
-      >
-        {`
-          (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "wtlhvpm4em");
-        `}
-      </Script>
-      {process.env.NODE_ENV === "production" && (
-        <GoogleAnalytics gaId="G-F7XL718W47" />
+        {process.env.NODE_ENV === "production" && (
+        <>
+          <Script id="microsoft-clarity" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wtlhvpm4em");
+            `}
+          </Script>
+
+          <GoogleAnalytics gaId="G-F7XL718W47" />
+        </>
       )}
+      </body>
+      
+
       
     </html>
   );
