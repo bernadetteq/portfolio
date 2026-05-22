750339316965b0f8e459fd8a92610e2ac3b05866
Bernadette Quitoriano <bernadettequitoriano@mac.speedport.ip>
2026-05-22 02:45:48 +0200

    complete certification list

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,44 @@
+import Link from "next/link";
+const certifications = [
+  {
+    logo: "/images/sa-csm-600.png",
+    title: "Certified Scrum Master",
+    issuer: "Scrum Alliance",
+    link: "https://www.scrumalliance.org/members/1397795",
+  },
+  {
+    logo: "/images/sa-cspo-600.png",
+    title: "Certified Scrum Product Owner",
+    issuer: "Scrum Alliance",
+    link: "https://www.scrumalliance.org/members/1397795",
+  },
+  {
+    logo: "/images/baymard-ux-professional.png",
+    title: "Baymard Institute UX Professional",
+    issuer: "Baymard Institute",
+    link: "https://baymard.com/certified/ffd6",
+  },
+  {
+    logo: "/images/duke-university.png",
+    title: "AI Produt Management",
+    issuer: "Coursera",
+    link: "https://www.coursera.org/account/accomplishments/specialization/AW1HS7MSSJX6",
+  },
+
+  {
+    logo: "/images/aipmm.png",
+    title: "Certified Product Manager",
+    issuer: "Association of International Product Marketing & Management",
+    link: "https://certificates.aipmm.com/44810342-7961-47d0-ab59-b49be3d04027#acc.zC7jk5cI",
+  },
+  {
+    logo: "/images/freecodecamp.png",
+    title: "Legacy Responsive Web Design Certification",
+    issuer: "freeCodeCamp",
+    link: "https://www.freecodecamp.org/certification/fcc84a02b8a-8eae-4633-90ce-b61de8c35fd0/responsive-web-design",
+  },
+  
+];
 export default function AboutPage() {
   return (
     //<main className="py-24 max-w-3xl">
@@ -67,7 +108,7 @@ export default function AboutPage() {
                 <div className="border rounded-2xl p-6">
                     <h3 className="text-xl tracking-tight">UX Design</h3>
                     <p className="text-gray-500 mt-2">
-                    During my time in front-end development, I naturally gravitated toward UX design, which has been incredibly valuable in my journey as a Product Owner. Combined with my experience in back-end development and web design, it’s given me a well-rounded perspective on building user-centered products.
+                    During my time in front-end development, I naturally gravitated toward UX design, which has been incredibly valuable in my journey as a Product Owner. Combined with my experience in back-end development and web design, it's given me a well-rounded perspective on building user-centered products.
                     </p>
                 </div>
                 <div className="border rounded-2xl p-6">
@@ -78,6 +119,141 @@ export default function AboutPage() {
                 </div>
             </div>
         </section>
+        <section className="space-y-8">
+
+            {/* SECTION HEADER */}
+            <h2 className="text-4xl tracking-tight mb-8">Certifications</h2>
+
+            {/* FLEX WRAP LAYOUT */}
+            <div
+                className="
+                flex
+                flex-wrap
+                justify-center
+                gap-6
+                "
+            >
+
+                {certifications.map((cert, index) => (
+
+                <Link
+                    key={index}
+                    href={cert.link}
+                    target="_blank"
+                    className="
+                    w-full
+                    sm:w-[200px]    
+
+                    
+
+                    
+
+                    flex
+                    flex-col
+                    items-center
+                    text-center
+
+                    
+                    hover:-translate-y-1
+
+                    transition-all
+                    duration-300
+                    "
+                >
+
+                    {/* BADGE */}
+                    <div
+                    className="
+                        w-50
+                        h-50
+                        
+
+                        flex
+                        items-center
+                        justify-center
+
+                        overflow-hidden
+
+                        mb-5
+                    "
+                    >
+
+                    <img
+                        src={cert.logo}
+                        alt={cert.title}
+                        className="
+                        w-40
+                        h-40
+                        object-contain
+                        "
+                    />
+
+                    </div>
+
+                    {/* TITLE */}
+                    <h3
+                    className="
+                        text-sm
+                        font-semibold
+                        leading-snug
+                    "
+                    >
+                    {cert.title}
+                    </h3>
+
+                    {/* ISSUER */}
+                    <p
+                    className="
+                        text-xs
+                        text-zinc-500
+                        mt-2
+                    "
+                    >
+                    {cert.issuer}
+                    </p>
+
+                </Link>
+
+                ))}
+
+            </div>
+
+        </section>
+
+        {/* <section className="space-y-8">
+            <h2 className="text-4xl tracking-tight mb-8">Certifications</h2>
+
+            <div className="space-y-6">
+                <div className="flex flex-col sm:flex-row">
+                <div className="w-full sm:w-1/4 flex-none space-y-4 pr-8">
+                    <div
+                      className="
+                        flex-shrink-0
+                        w-14
+                        h-14
+                        rounded-2xl
+                        bg-zinc-100
+                        flex
+                        items-center
+                        justify-center
+                        overflow-hidden
+                      "
+                    >
+                      <img
+                        src="/images/sa-cspo-600.png"
+                        alt="Certified Scrum Product Owner Logo"
+                        className="w-10 h-10 object-contain"
+                      />
+                    </div>
+                </div>
+                <div className="w-fullsm:w-1/4 flex-none space-y-4 pr-8">
+                    
+                </div>
+                <div className="w-fullsm:w-1/4 flex-none space-y-4 pr-8 "></div>
+                <div className="w-fullsm:w-1/4 flex-none space-y-4 pr-8 "></div>
+            </div>
+            </div>
+        </section> */}
     </main>
   );
 }
