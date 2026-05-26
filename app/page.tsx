8344fb06263341adb7784fa20282be3b929edaa3
Bernadette Quitoriano <bernadettequitoriano@mac.speedport.ip>
2026-05-26 16:36:08 +0200

    update hover links

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
         <div className="space-y-6">
           <Link href="/projects/digitizing-returns">
-            <div className="border rounded-2xl p-6 mb-8 hover:translate-x-[10px] transition-transform duration-300">
+            <div className="border border-gray-500 rounded-2xl p-6 mb-8 hover:translate-x-[10px] transition-transform duration-300">
               <h3 className="text-xl tracking-tight">Digitizing the Returns Experience</h3>
               <p className="text-gray-500 mt-2">
                 Built a self-service returns platform that addressed rising support demand, streamlined operations, and improved the end-to-end customer experience.
@@ -41,7 +41,7 @@ export default function Home() {
             </div>
           </Link>
           <Link href="/projects/scaling-return-operations">
-            <div className="border rounded-2xl p-6 mb-8 hover:translate-x-[10px] transition-transform duration-300">
+            <div className="border border-gray-500 rounded-2xl p-6 mb-8 hover:translate-x-[10px] transition-transform duration-300">
               <h3 className="text-xl tracking-tight">Scaling Return Operations Through Automation</h3>
               <p className="text-gray-500 mt-2">
                 Introduced automated return tracking and notifications to reduce operational overhead and accelerate customer access to real-time return information.
@@ -49,7 +49,7 @@ export default function Home() {
             </div>
           </Link>
           <Link href="/projects/quotation-platform-product-discovery">
-            <div className="border rounded-2xl p-6 hover:translate-x-[10px] transition-transform duration-300">
+            <div className="border border-gray-500 rounded-2xl p-6 hover:translate-x-[10px] transition-transform duration-300">
               <h3 className="text-xl tracking-tight">Shaping the Next Phase of an Internal Quotation Tool</h3>
               <p className="text-gray-500 mt-2">
                 Used research, stakeholder interviews, and workflow analysis to identify high-impact enhancements and inform future product direction.
